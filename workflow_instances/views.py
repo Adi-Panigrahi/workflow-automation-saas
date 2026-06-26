67f7f940c2b58e4290048a82f6a0c3b77d5a5fcf
Aditya Panigrahi <aditya.panigrahi412@gmail.com>
2026-06-26 20:32:49 +0530

    Add worflow instance class

--- workflow_instances/views.py
+++ workflow_instances/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import WorkflowInstance
+from .serializers import WorkflowInstanceSerializer
 
-# Create your views here.
+
+class WorkflowInstanceViewSet(viewsets.ModelViewSet):
+    queryset = WorkflowInstance.objects.all()
+    serializer_class = WorkflowInstanceSerializer
