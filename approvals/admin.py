7d5ac29f6af5a573de1fd093f559d213292ca10b
Aditya Panigrahi <aditya.panigrahi412@gmail.com>
2026-07-01 01:10:05 +0530

    Add approval domain model for workflow engine

--- approvals/admin.py
+++ approvals/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Approval
 
-# Register your models here.
+
+@admin.register(Approval)
+class ApprovalAdmin(admin.ModelAdmin):
+    list_display = (
+        "workflow_instance",
+        "workflow_step",
+        "assigned_to",
+        "status",
+        "approved_at",
+    )
+
+    list_filter = (
+        "status",
+    )
+
+    search_fields = (
+        "assigned_to__email",
+    )
